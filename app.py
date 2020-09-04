ca77937fcfc92229d5c5c0b2e6eb1c7cc8d54ce9
Gary Kuo <gary19910201@gmail.com>
2020-09-04 22:37:50 +0800

    update2

--- app.py
+++ app.py
@@ -38,10 +38,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '熊熊愛你'
+    r = '熊熊看不懂你說什麼'
+
+    if msg == 'hi':
+        r = 'hi'
+    elif msg == '熊熊':
+        r = '熊熊說你好'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
